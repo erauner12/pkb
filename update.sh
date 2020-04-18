50323016f66215668dcfe4624a10d12523fbbe8c
Evan <raunerevan@gmail.com>
2020-04-18 02:32:44 -0500

    Regular update

--- update.sh
+++ update.sh
@@ -13,9 +13,10 @@ find . -type f -name "*.org" -exec sed -i 's/png\]\]/png/g' {} \;
 # exports all org files to html
 orgmk -r
 
-# find all html files and do not incude this directory or the two other below it and 
-#find . -mindepth 3 -name '*.html' -print 
+# find all html files and do not incude this directory or the two other below it and insert jekyll matters title so it can be searched
+find ./* -mindepth 2 -regex ".*\.\(html\)" -type f | xargs sed -i '1 i\ ---\ntitle: "title"\n--- '
 
+# | cat - {} > {} \;
 
 ####### write a script that finds instances of pictures you need to find and replace on within the lines before you commit
 
@@ -25,7 +26,3 @@ git add -A
 git commit -m "Regular update"
 git push
 
-
-
-
-#echo "$(echo 'task goes here' | cat - todo.txt)" > todo.txt
