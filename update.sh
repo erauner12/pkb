fa4b185746ff1d95c0888d4c7d9798a415de6cf3
Evan <raunerevan@gmail.com>
2020-04-18 01:53:06 -0500

    Regular update

--- update.sh
+++ update.sh
@@ -13,6 +13,10 @@ find . -type f -name "*.org" -exec sed -i 's/png\]\]/png/g' {} \;
 # exports all org files to html
 orgmk -r
 
+# find all html files and do not incude this directory or the two other below it and 
+#find . -mindepth 3 -name '*.html' -print 
+
+
 ####### write a script that finds instances of pictures you need to find and replace on within the lines before you commit
 
 
@@ -20,3 +24,8 @@ orgmk -r
 git add -A
 git commit -m "Regular update"
 git push
+
+
+
+
+#echo "$(echo 'task goes here' | cat - todo.txt)" > todo.txt
